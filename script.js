67641d79c27f77ae389957669a0a1995e79ece58
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-26 19:52:33 +0200

    Access to the id from the html document

---
+++ script.js
@@ -0,0 +1,5 @@
+// Access to the id from the html document
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const result = document.getElementById("results-div");
