7893bdc70fc183b801317959baddd7664c57bd23
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-27 08:40:53 +0200

    addEventListener( ) checkBtn + clearBtn

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const userInput = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const result = document.getElementById("results-div");
+
+// addEventListener() checkBtn 
+checkBtn.addEventListener("click",() => {
+    if(!userInput.value){
+        alert("Please provide a phone number");
+    }
+})
+
+// addEventListener() clearBtn 
+clearBtn.addEventListener("click", () => result.innerText = "");
