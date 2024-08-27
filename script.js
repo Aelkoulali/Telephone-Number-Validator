dabb17f9a51124199a2a1693376a06bb0a2b1ca7
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-27 10:40:43 +0200

    Declare regex variable and test

--- script.js
+++ script.js
@@ -6,10 +6,27 @@ const result = document.getElementById("results-div");
 
 // addEventListener() checkBtn 
 checkBtn.addEventListener("click",() => {
-    if(!userInput.value){
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
+
+    if(!userInput.value) {
         alert("Please provide a phone number");
-    }
-})
+    } else if (regex.test(userInput.value)){
+        result.innerText = `Valid US number: ${userInput.value}`;
+    } else {
+        result.innerText = `Invalid US number: ${userInput.value}`;
+    }  
+});
 
 // addEventListener() clearBtn 
 clearBtn.addEventListener("click", () => result.innerText = "");
+
+
+/*
+Regex:
+Optional "1" with a space => (1\s?)?
+3 digits with optional ( ) => (\(\d{3}\)|\d{3})
+Optional - or space => ([\s-]?)
+3 digits => \d{3}
+Optional - or space => ([\s-]?)
+4 digits => \d{4}
+*/
